1f9e5d84227145124e01e59d6e0d8ec42fa7e921
KenjiMobutu <kenjimobutu@gmail.com>
2023-07-24 22:52:06 +0200

    header

--- client/src/pages/Index.jsx
+++ client/src/pages/Index.jsx
@@ -1,10 +1,12 @@
 import "./index.css"
 import Navbar from "../components/navbar/Navbar";
+import Header from "../components/header/Header";
 
 export default function Index(){
   return(
     <div>
       <Navbar/>
+      <Header/>
     </div>
   );
 }
