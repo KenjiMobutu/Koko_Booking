b6ef83ea4532a4c7fcb938cb2f621a1af0e4fd8c
KenjiMobutu <kenjimobutu@gmail.com>
2023-07-26 21:55:59 +0200

    property List

--- client/src/pages/Index.jsx
+++ client/src/pages/Index.jsx
@@ -2,6 +2,7 @@ import "./index.css"
 import Navbar from "../components/navbar/Navbar";
 import Header from "../components/header/Header";
 import Featured from "../components/featured/Featured";
+import PropertyList from "../components/propertyList/PropertyList";
 
 export default function Index(){
   return(
@@ -10,7 +11,8 @@ export default function Index(){
       <Header/>
       <div className="homeContainer">
         <Featured/>
-        
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList/>
       </div>
     </div>
   );
